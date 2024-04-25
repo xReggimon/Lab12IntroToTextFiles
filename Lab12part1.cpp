4f446eb37994b5aba093797c7180fd03c2c9ff66
xReggimon <r-mcmullen.1@onu.edu>
2024-04-25 08:36:25 -0400

    Added pressEnterToContinue

--- Lab12part1.cpp
+++ Lab12part1.cpp
@@ -2,6 +2,17 @@
 #include <fstream>
 using namespace std;
 
+
+
+void pressEnterToContinue();
+
 int main() {
 	cout << "Lab 12 Intro to Text Files!\n";
+	pressEnterToContinue;
+}
+
+void pressEnterToContinue() {
+	cout << "\nPress enter to continue... \n";
+	cin.clear();
+	while (cin.get() != '\n') continue;
 }
